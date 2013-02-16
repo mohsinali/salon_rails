5c2f24664fcdf8e49f2c75f9b7385290ecdf70f3
Mohsin <mohsin@mohsin-laptop.(none)>
2013-02-16 22:53:01 +0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salon::Application.config.secret_token = '755e3fe42580a090137d101a50438a1cb02a239c10fc79506941b653f6be4921f5f0e03d35a5257dcf43b5b7e36b482dc63cf81ecf38e407c0e7a80b3f567a32'
